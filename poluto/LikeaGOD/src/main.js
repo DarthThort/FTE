b3dcb708bcceed7233e2dc662e12fcaa179a663d
Luis Marchante <darthluis@gmail.com>
2025-11-24 09:22:53 +0100

    basura borrada

--- poluto/LikeaGOD/src/main.js
+++
@@ -1,7 +0,0 @@
-import { Game } from './core/Game.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('game-canvas');
-    const game = new Game(canvas);
-    game.start();
-});
