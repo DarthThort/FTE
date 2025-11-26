f5b584795dc7c5308be130e95c4b9d09c827a108
Luis Marchante <darthluis@gmail.com>
2025-11-26 21:20:52 +0100

    Debug: Added logging to weapon-refresh to diagnose refresh issue

--- Solitary-chromosphere/js/ui/weapon-refresh.js
+++ Solitary-chromosphere/js/ui/weapon-refresh.js
@@ -3,9 +3,12 @@ setInterval(() => {
     const panel = document.getElementById('weapons-panel');
     if (panel) {
         // Get the game instance from window
-        const game = window.gameEngine || window.game;
+        const game = window.game;
         if (game && game.ui && game.ui.weaponUI) {
+            console.log('[weapon-refresh] Refreshing weapon panel');
             game.ui.weaponUI.refreshWeaponsPanel();
+        } else {
+            console.log('[weapon-refresh] Could not find game.ui.weaponUI');
         }
     }
 }, 100);
