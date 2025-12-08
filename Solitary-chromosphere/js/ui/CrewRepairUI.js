f1e3af30aedf8035a554f86805447309e2dfc7cb
Luis Marchante <darthluis@gmail.com>
2025-12-08 10:04:10 +0100

    Remove teleport hack - restore normal crew walking to breaches

--- Solitary-chromosphere/js/ui/CrewRepairUI.js
+++ Solitary-chromosphere/js/ui/CrewRepairUI.js
@@ -256,17 +256,14 @@ class CrewRepairUI {
             console.log(`[CrewRepairUI] Unassigned ${crewMember.name} from ${assignedSystem.type} system`);
         }
 
-        // HACK: Teleport crew to breach and start repairing immediately (bypassing movement)
-        crewMember.x = breach.x * 32 + 16;
-        crewMember.y = breach.y * 32 + 16;
+        // Set crew to move to breach location
         crewMember.targetBreach = breachIndex;
         crewMember.targetX = breach.x * 32 + 16;
         crewMember.targetY = breach.y * 32 + 16;
-        crewMember.state = 'repairing';  // GO STRAIGHT TO REPAIRING
-        crewMember.repairProgress = 0;
+        crewMember.state = 'moving';  // Crew will walk to breach
         crewMember.path = [];
 
-        console.log(`[CrewRepairUI] 🔧 HACK: ${crewMember.name} TELEPORTED to breach and REPAIRING NOW`);
+        console.log(`[CrewRepairUI] Assigned ${crewMember.name} to breach at (${breach.x}, ${breach.y}) - crew will walk there`);
 
         this.hide();
     }
